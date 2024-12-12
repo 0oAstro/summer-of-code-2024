60440122de724dd73a2e4b868566df20f41df034
Shaurya Pratap Singh (Astro) <ee1240486@iitd.ac.in>
2024-12-12 18:34:56 +0530

    add login page

--- flake.nix
+++ flake.nix
@@ -19,14 +19,11 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           venvDir = ".venv";
-          packages = with pkgs; [ poetry python311 postgresql ] ++ (with python311Packages; [
+          packages = with pkgs; [ poetry python311 ] ++ (with python311Packages; [
             ipykernel
             pip
             venvShellHook
           ]);
-          shellHook = ''
-            export PGDATA=data
-          '';
         };
       });
     };
